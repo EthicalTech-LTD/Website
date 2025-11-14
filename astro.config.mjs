8b2c62d17c74d66c9872a8c076ca455349b9411a
EthicalTech UK <ethicaltechuk@proton.me>
2025-11-14 01:15:26 +0000

    Initial setup of tailwind landing page.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
+import tailwind from "@astrojs/tailwind";
+
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [solidJs()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
