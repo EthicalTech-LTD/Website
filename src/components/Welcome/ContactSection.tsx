4c69e2e1fbee204fd86b570fd576598f9f62056c
EthicalTech UK <ethicaltechuk@proton.me>
2025-11-14 14:36:56 +0000

    Adjust hover button state

--- src/components/Welcome/ContactSection.tsx
+++ src/components/Welcome/ContactSection.tsx
@@ -29,7 +29,7 @@ export default function ContactSection() {
                 <path d="m12 5 7 7-7 7"></path>
               </svg>
             </button>
-            <button class="inline-flex items-center justify-center gap-2 whitespace-nowrap font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground h-10 rounded-md text-lg px-10">
+            <button class="inline-flex items-center justify-center gap-2 whitespace-nowrap font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 border border-input bg-background shadow-sm hover:bg-emerald-100 hover:text-accent-foreground h-10 rounded-md text-lg px-10">
               View our pricing
             </button>
           </div>
