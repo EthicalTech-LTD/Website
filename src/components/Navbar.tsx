55280bdb992fcdfba0fb7d42f09d3366c1e64559
EthicalTech UK <ethicaltechuk@proton.me>
2025-11-17 13:13:41 +0000

    Turn navbar brand text into home link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,9 +19,9 @@ export default function Navbar() {
               <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path>
             </svg>
           </div>
-          <span class="text-2xl font-bold brand text-emerald-800">
+          <a href="/" class="text-2xl font-bold brand text-emerald-800">
             EthicalTech UK
-          </span>
+          </a>
         </div>
         <nav class="hidden md:flex items-center gap-8">
           <a
